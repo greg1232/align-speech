f5d55ad38efc29ee2e72bf3496d49d12f1eef965
greg <gregory.diamos@gmail.com>
2021-12-25 17:30:20 -0800

    BUG: start/end checks.

--- align_speech/api/align.py
+++ align_speech/api/align.py
@@ -35,14 +35,18 @@ def align_pairs(individual_alignments, config):
             continue
 
         # fix previous alignments that run over the current
-        if previous["end"] > current["start"]:
+        if previous["end"] > current["start"] and previous["start"] < current["start"]:
             logger.debug("Fixing overlapping alignments: ")
             logger.debug(" Previous: " + str(previous))
             logger.debug(" Current:" + str(current))
             previous["end"] = current["start"]
 
         # split the difference between previous and current
-        midpoint = current["start"] + (current["start"] - previous["end"]) / 2
+        midpoint = previous["end"] + (current["start"] - previous["end"]) / 2
+
+        logger.debug("Adjusting midpoint: " + str(midpoint))
+        logger.debug(" Previous: " + str(previous))
+        logger.debug(" Current:" + str(current))
 
         previous["end"] = midpoint
         current["start"] = midpoint
